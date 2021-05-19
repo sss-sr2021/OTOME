cba0719360b7d5e312ebccf3428da4fa95c489da
豊島 <yuka_t@sr-co.co.jp>
2021-05-19 16:08:01 +0900

    HTML

---
+++ www/HEALPY_code/check.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>健康チェックアンケート</title>
+    <link rel="stylesheet" href="css/style.css" media="screen and (min-width:1024px)"> <!--画面サイズ1024px以上はこのファイルはスタイルが適用される -->
+</head>
+<body>
+    <!-- ヘッダー -->
+    <header>
+        <h1>HEALPY</h1>
+        <nav class="nav">
+            <ul>
+            <li><a href="index.php">TOP</a></li>
+            <li><a href="check_top.php">健康チェック</a></li>
+            <li><a href="recipe_top.php">レシピ</a></li>
+            <li><a href="mypage_top.php">マイページ</a></li>
+            <li><img src="img/logout.png" width="45px" title="ログアウト"></li>
+        </ul>
+        </nav>
+    </header>
+ 
+    <!-- メインコンテンツ -->
+    <main>
+        <h3>ページタイトル</h3>
+        中身
+    </main>
+ 
+    <!-- フッター -->
+    <footer>
+    <nav class="nav">
+            <ul id=footer_nav>
+            <li><a href="policy.php">プライバシーポリシー</a></li>
+            <li><a href="service.php">利用規約表示画面</a></li>
+            <li><a href="company.php">運営会社</a></li>
+        </ul>
+        </nav>
+    </footer>
+</body>
+</html>
