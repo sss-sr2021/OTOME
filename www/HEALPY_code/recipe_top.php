723e3f6fc8f6d4e3be43c0636912fae891775430
tamaki0121 <tamaki@sr-co.co.jp>
2021-05-24 17:45:06 +0900

    変更

--- www/HEALPY_code/recipe_top.php
+++ www/HEALPY_code/recipe_top.php
@@ -1,21 +1,46 @@
-<!-- 
-recioe_top.php：レシピページ
-Autor：豊島
-Virsion：0.0.1
-Crated：2021.05.19
-Update：2021.05.20
- -->
- <?php include_once('header.php')?>
- 
- <!-- メインコンテンツ -->
- <main>
-     <h3>レシピ</h3>
-     おすすめレシピをご紹介
- </main>
 
- <!-- フッター -->
- <?php include_once('footer.php')?>
+<?php
+/*** 
+*recipe_top.php : ログイン
+*
+*Author : 豊島、畑本
+*Version : 0.0.1
+*Created : 2021.05.19
+*Update : 2021.05.20 24(畑本)
+*
+*/
+
+require_once 'function.php';
+$dbh = dbInit();
+
+/*
+* idをランダムに発生させる
+*/
+$id = mt_rand(1,20);
+
+/*
+* 画像ファイルを取得
+*/
+$sql = "SELECT recipe_file FROM recipe WHERE id = :id";
+$sth = $dbh->prepare($sql);
+$ret = $sth->execute([
+    'id' => $id
+]);
+$rows = $sth->fetchAll();
+
+?>
+<?php include_once('header.php')?>
+
+<!-- メインコンテンツ -->
+<main>
+    <h3>レシピ</h3>
+    おすすめレシピをご紹介
+    <img src="recipe/<?= $rows[0]['recipe_file'] ?>" width="90%" />
+</main>
+
+<!-- フッター -->
+<?php include_once('footer.php')?>
 
 <script>
- document.getElementById('title').innerHTML="レシピ";
+document.getElementById('title').innerHTML="レシピ";
 </script>
