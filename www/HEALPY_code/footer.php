85701b2de245c5738b236131128106ed0df61e0b
豊島 <yuka_t@sr-co.co.jp>
2021-05-25 17:44:41 +0900

    編集

--- www/HEALPY_code/footer.php
+++ www/HEALPY_code/footer.php
@@ -8,13 +8,13 @@ Update：2021.05.20
     
 <footer class="pc">
         <nav class="nav">
-                <ul id=footer_nav>
+                <ul>
                 <li><a href="policy.php">プライバシーポリシー</a></li>
                 <li><a href="service.php">利用規約表示画面</a></li>
                 <li><a href="company.php">運営会社</a></li>
             </ul>
         </nav>
-        <p>Copyright © 株式会社システムリサーチ チーム乙女. All rights reserved.</p>
+        <p id=footer_nav>Copyright © 株式会社システムリサーチ チーム乙女. All rights reserved.</p>
     </footer>
 </body>
 </html>
